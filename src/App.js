0bbcb3a968d0785d9e12bfd05436ca8f27b4fd11
Felipe Passos <felipekaires@hotmail.com>
2018-07-02 16:18:55 -0300

    Cors updated

--- src/App.js
+++ src/App.js
@@ -1,102 +1,81 @@
-import React, { Component } from "react";
-import Header from "./components/template/header";
-import Search from "./components/template/search";
-import axios from "axios";
-import _ from "lodash";
+import React, { Component } from 'react';
+import Header from './components/template/header'
+import Search from './components/template/search'
+import axios from 'axios'
+import _ from 'lodash'
 
-class App extends Component {
+class App extends Component  {
+
   state = {
-    data: [],
-    artistName: "",
-    query: " "
-  };
+    data : [],
+    artistName: '',
+    query: ' ',
+  }
   handleKey = this.handleKey.bind(this);
 
-  handleKey(e) {
-    this.setState({
-      query: e.target.value
-    });
-  }
-  shouldComponentUpdate(state, nextState) {
-    return state.data !== nextState.data;
-  }
-  handleEnter = e => {
-    if ((e.key === "Enter") | (e.keywich === 13) | (e.keyCode === 13)) {
-      const search = this.state.query.split(" ").join("+");
-      const api = "https://itunes.apple.com/search?media=music&term=";
-      const to_fetch = api + search + "&limit=1&wrapperType=track&kind=song";
-      axios({
-        method: "get",
-        url: to_fetch,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "POST, GET, PUT, OPTIONS, DELETE",
-          "Access-Control-Allow-Headers": "x-requested-with, content-type"
-        }
+  handleKey(e)
+  {
+      this.setState({
+        query: e.target.value
       })
-        .then(response => {
-          if (!response.data || !response.data.resultCount) return false;
-          const artistId = response.data.results[0].artistId;
-          this.setState({
-            artistName: response.data.results[0].artistName.toUpperCase()
-          });
-          return axios({
-            method: "get",
-            url: `https://itunes.apple.com/lookup?id=${artistId}&entity=album`,
-            headers: {
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Methods": "POST, GET, PUT, OPTIONS, DELETE",
-              "Access-Control-Allow-Headers": "x-requested-with, content-type"
-            }
-          });
-        })
-        .then(response => {
-          if (!response.data || response.data.resultCount < 2) return false;
-          const data = response.data.results.slice(1);
-          let tracks = [];
-          for (let i = 0; i < data.length; i++) {
-            axios({
-              method: "get",
-              url: `https://itunes.apple.com/lookup?id=${
-                data[i].collectionId
-              }&entity=song`,
-              headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods":
-                  "POST, GET, PUT, OPTIONS, DELETE",
-                "Access-Control-Allow-Headers": "x-requested-with, content-type"
-              }
-            }).then(response => {
-              tracks = tracks.concat(response.data.results);
-              if (i === data.length - 1) {
-                const newdata = data;
-                this.setState(state => {
-                  if (state.data === newdata) {
-                    return null;
-                  } else {
-                    return {
-                      data: _.groupBy(tracks, "collectionName")
-                    };
+      
+  }
+  shouldComponentUpdate(state,nextState)
+  {
+    return state.data !== nextState.data
+  }
+  handleEnter = (e) => {
+  if ((e.key === 'Enter') | (e.keywich === 13) | (e.keyCode === 13)){ 
+   const search =  this.state.query.split(' ').join('+')
+   const api = "https://sheltered-caverns-93018.herokuapp.com/https://itunes.apple.com/search?media=music&term="
+   const to_fetch = api + search + "&limit=1&wrapperType=track&kind=song"
+   axios(to_fetch)
+    .then(response => {
+      if (!response.data || !response.data.resultCount) return false;
+      const artistId = response.data.results[0].artistId
+      this.setState({
+        artistName: response.data.results[0].artistName.toUpperCase()
+      })
+      return axios(`https://sheltered-caverns-93018.herokuapp.com/https://itunes.apple.com/lookup?id=${artistId}&entity=album`)
+    })
+    .then(response => {
+      if (!response.data || (response.data.resultCount < 2)) return false;
+      const data = response.data.results.slice(1)
+      let tracks = []
+      for (let i = 0; i < data.length; i++) {
+        axios(`https://sheltered-caverns-93018.herokuapp.com/https://itunes.apple.com/lookup?id=${data[i].collectionId}&entity=song`).then((response) => {
+        tracks = tracks.concat(response.data.results)  
+          if(i === (data.length - 1)){
+            const newdata = data
+              this.setState(state =>{
+                if(state.data === newdata){
+                  return null;
+                } else{
+                  return {
+                    data : _.groupBy(tracks,'collectionName'),
                   }
-                });
-              }
-            });
+                }
+              })    
           }
         });
-    }
-  };
+      }
+    })}
+   
+  }
   render() {
     return (
       <div>
-        <Header
-          artistName={this.state.artistName}
-          handleKey={this.handleKey}
-          handleEnter={this.handleEnter}
+        <Header 
+        artistName = {this.state.artistName}
+        handleKey={this.handleKey}
+        handleEnter={this.handleEnter}
         />
-        <Search search={this.state.data} />
+        <Search
+          search={this.state.data}
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
